1a3306cd2269a753998edeb6bd81c319700323b5
Paht J <paht.juangphanich@gmail.com>
2021-03-08 20:41:08 -0500

    still weird but okay

--- Connectivity.jl
+++ Connectivity.jl
@@ -1,7 +1,8 @@
 module Connectivity
     include("DataTestTypes.jl") # How do I make it obvious that block comes from DataTestTypes
-    using .DataTestTypes: Block
-    export add_blocks 
+    # using .DataTestTypes: Block
+    import .DataTestTypes: Block
+    export add_blocks, Block
     
     function add_blocks(block1::Block, block2::Block)
         return block1.a + block2.b
--- run_me.jl
+++ run_me.jl
@@ -1,9 +1,11 @@
 import Pkg
 
 include("Connectivity.jl")
-using .Connectivity: Block # it is not obvious without digging into the code that connectivity exports Block
+# include("DataTestTypes.jl")
+import .Connectivity: Block, add_blocks # it is not obvious without digging into the code that connectivity exports Block
+# import .DataTestTypes: Block
 
 b1 = Block(1,2)
 b2 = Block(1,2)
-result = Connectivity.add_blocks(b1,b2)
+result = add_blocks(b1,b2)
 print(result)
